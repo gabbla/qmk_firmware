a0f196f70c3710e644de652d734e7512abd31960
Gabriele Zampieri <gabriele.zampieri@texa.com>
2021-04-26 15:11:33 +0200

    reviung41: gab: fixing layout

--- keyboards/reviung41/keymaps/gab/keymap.c
+++ keyboards/reviung41/keymaps/gab/keymap.c
@@ -27,10 +27,13 @@ enum layer_names {
 #define RAISE  MO(_RAISE)
 #define ADJUST MO(_ADJUST)
 
+#define CTESC LCTL_T(KC_ESC)
+#define ENTSH RSFT_T(KC_ENT)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_reviung41(
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
-    KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+    CTESC,    KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  RSFT_T(KC_ENT),
                                             KC_LGUI,   LOWER,    KC_SPC,   RAISE,    KC_LALT
   ),
@@ -50,9 +53,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   
   [_ADJUST] = LAYOUT_reviung41(
-    RGB_VAI,   RGB_SAI, RGB_HUI,  RGB_MOD,  XXXXXXX,   RGB_TOG,            KC_MNXT,  KC_VOLD,  KC_VOLU,  KC_MPLY,  XXXXXXX,  XXXXXXX,
+    RGB_VAI,   RGB_SAI, RGB_HUI,  RGB_MOD,  XXXXXXX,   RGB_TOG,            KC_MNXT,  KC_VOLD,  KC_VOLU,  KC_MPLY,  XXXXXXX,  KC_DEL,
     RGB_VAD,   RGB_SAD, RGB_HUD,  RGB_RMOD, XXXXXXX,   XXXXXXX,            KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   RESET,              XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   RESET,              XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,
                                             _______,   _______,  XXXXXXX,  _______,  _______
   ),
 };
